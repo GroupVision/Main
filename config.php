a5dc6a98bf46d90583d6efd9b21f78e79fb17423
gustavoBinder <gustavo.binderalmeida@gmail.com>
2022-04-20 20:07:50 -0300

    config sql

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 
-    $dbHost = 'localhost';
-    $dbUsername = 'root';
-    $dbPassword = '';
+    $dbHost = 'us-cdbr-east-05.cleardb.net';
+    $dbUsername = 'b220b114472b51';
+    $dbPassword = 'a215c1d8';
     $dbName = 'ods_db';
+    $database = 'heroku_5491e5553e154f5';
     
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName,$database);
 
     // if($conexao->connect_errno)
     // {
