172de570df14010453e29c386f1ca3789b3a15dc
gustavoBinder <gustavo.binderalmeida@gmail.com>
2022-04-20 19:53:03 -0300

    merge request - devop

--- faq.php
+++ faq.php
@@ -5,11 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD:faq.html
   <title>Landing Page Templaaaate</title>
-=======
-  <title>Landing Page </title>
->>>>>>> develop:faq.php
   <link rel="shortcut icon" href="image/Logo.svg" type="image/x-icon">
   <!-- Bootstrap , fonts & icons  -->
   <link rel="stylesheet" href="css/bootstrap.css">
